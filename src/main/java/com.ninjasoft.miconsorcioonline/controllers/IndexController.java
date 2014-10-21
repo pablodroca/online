0c9a71b4d04824e17dc0e8e39e84e09b657ba51a
Pablo D. Roca <pablodroca@gmail.com>
2014-10-20 22:37:46 -0300

    Add base structure for angular and appengine

---
+++ src/main/java/com.ninjasoft.miconsorcioonline/controllers/IndexController.java
@@ -0,0 +1,1 @@
+package com.ninjasoft.miconsorcioonline.controllers;import javax.ws.rs.GET;import javax.ws.rs.Path;import javax.ws.rs.Produces;import javax.ws.rs.core.MediaType;@Path("/index")public class IndexController {    @GET    //@Produces(MediaType.APPLICATION_JSON)    public String getUsername() {        return "pablodroca";    }}
